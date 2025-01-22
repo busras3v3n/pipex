b28ae0c651b7ea69ee4036305f9f2c65bf312553
Büşra Seven <152426280+busras3v3n@users.noreply.github.com>
2025-01-22 19:58:22 +0300

    minor changes

--- pipex.c
+++ pipex.c
@@ -30,10 +30,16 @@ char	**extract_env_path(char **env)
 char	*find_correct_path(char **cmd_arr, char **paths)
 {
 	int i = 0;
+	char *path;
 	while(paths[i])
 	{
-		ft_st
+		path = ft_join(cmd_arr[0], paths[i]);
+		if(access(path, F_OK | X_OK) == 0);
+			break ;
+		free(path);
+		i++;
 	}
+	return (path);
 }
 int	main(int argc, char **argv, char **env)
 {
@@ -43,19 +49,14 @@ int	main(int argc, char **argv, char **env)
 	if(argc == 5 && argv && env[i])
 	{
 		paths = extract_env_path(env);
-		while(paths[i])
-		{
-			ft_printf("%s\n", paths[i]);
-			i++;
-		}
 		id = fork();
 		if(id == 0)
-			ft_printf("I am in child process\n");
-		else
 		{
-			wait(NULL);
-			ft_printf("parent process\n");
+			child_process(argv[1], );
 		}
+		else
+			parent_process(argv[2], );
+		ft_freeall(paths);
 	}
 	else
 		write(1, "incorrect number of arguments\n", 30);

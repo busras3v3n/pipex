99d790da110e9575a052d4ba405ad3cd4d962101
Büşra Seven <152426280+busras3v3n@users.noreply.github.com>
2025-01-26 12:05:08 +0300

    norme fixes

--- Mandatory/errors.c
+++ Mandatory/errors.c
@@ -6,7 +6,7 @@
 /*   By: busseven <busseven@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 13:57:17 by busseven          #+#    #+#             */
-/*   Updated: 2025/01/26 11:53:04 by busseven         ###   ########.fr       */
+/*   Updated: 2025/01/26 11:57:14 by busseven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,20 @@
 void	invalid_file(t_pipex *prog)
 {
 	ft_printf("Error\n");
-	if(prog->fd_infile < 0)
+	if (prog->fd_infile < 0)
 		ft_printf("invalid infile\n");
-	if(prog->fd_outfile < 0)
+	if (prog->fd_outfile < 0)
 		ft_printf("invalid outfile\n");
 	free_prog(prog);
 	exit(1);
 }
+
 void	invalid_command(t_pipex *prog)
 {
 	ft_printf("Error\n");
-	if(!prog->cmd_arr[0]->path)
+	if (!prog->cmd_arr[0]->path)
 		ft_printf("can't find path for %s\n", prog->cmd_arr[0]->arg_arr[0]);
-	if(!prog->cmd_arr[1]->path)
+	if (!prog->cmd_arr[1]->path)
 		ft_printf("can't find path for %s\n", prog->cmd_arr[1]->arg_arr[0]);
 	free_prog(prog);
 	exit(1);
--- Mandatory/pipex.c
+++ Mandatory/pipex.c
@@ -6,7 +6,7 @@
 /*   By: busseven <busseven@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 13:51:25 by busseven          #+#    #+#             */
-/*   Updated: 2025/01/26 11:50:35 by busseven         ###   ########.fr       */
+/*   Updated: 2025/01/26 12:04:02 by busseven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,40 @@
 #include <stdio.h>
 #include <signal.h>
 
+void	init_program(t_pipex *prog, char **argv, char **env)
+{
+	t_cmd	*cmd_0;
+	t_cmd	*cmd_1;
+
+	cmd_0 = prog->cmd_arr[0];
+	cmd_1 = prog->cmd_arr[1];
+	prog->fd_infile = open(argv[1], O_RDWR);
+	prog->fd_outfile = open(argv[4], O_RDWR);
+	prog->cmd_arr = ft_calloc(2, sizeof(t_cmd));
+	prog->paths = extract_env_path(env);
+	cmd_0 = ft_calloc(1, sizeof(t_cmd));
+	cmd_1 = ft_calloc(1, sizeof(t_cmd));
+	cmd_0->arg_arr = make_command_arr(argv[2]);
+	cmd_1->arg_arr = make_command_arr(argv[3]);
+	cmd_0->path = find_correct_path(cmd_0->arg_arr[0], prog->paths);
+	cmd_1->path = find_correct_path(cmd_1->arg_arr[0], prog->paths);
+	pipe(prog->fd);
+	if (prog->fd_infile < 0 || prog->fd_outfile < 0)
+		invalid_file(prog);
+	if (!prog->cmd_arr[0]->path || !prog->cmd_arr[1]->path)
+		invalid_command(prog);
+}
+
 void	process(int i, t_pipex *prog, char **env)
 {
-	if(i == 0)
+	if (i == 0)
 	{
 		dup2(prog->fd_infile, 0);
 		dup2(prog->fd[1], 1);
 		close(prog->fd[0]);
 		execve(prog->cmd_arr[0]->path, prog->cmd_arr[0]->arg_arr, env);
 	}
-	if(i == 1)
+	if (i == 1)
 	{
 		dup2(prog->fd_outfile, 1);
 		dup2(prog->fd[0], 0);
@@ -36,22 +60,24 @@ void	process(int i, t_pipex *prog, char **env)
 		execve(prog->cmd_arr[1]->path, prog->cmd_arr[1]->arg_arr, env);
 	}
 }
+
 int	main(int argc, char **argv, char **env)
 {
-	t_pipex *prog;
-	int id;
-	int i;
+	t_pipex	*prog;
+	int		id;
+	int		i;
+
 	i = 0;
 	id = 1;
-	if(argc == 5)
+	if (argc == 5)
 	{
 		prog = ft_calloc(1, sizeof(t_pipex));
 		init_program(prog, argv, env);
-		while(i <= 1)
+		while (i <= 1)
 		{
-			if(id != 0)
+			if (id != 0)
 				id = fork();
-			if(id == 0)
+			if (id == 0)
 				process(i, prog, env);
 			else
 				ft_printf("parent\n");

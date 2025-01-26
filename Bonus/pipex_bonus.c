de7553bfe8c6a1960fab8f7a50f72a6724c8026a
Busra Seven <busras3v3n@proton.me>
2025-01-26 14:57:51 +0300

    edited init program function for bonus

---
+++ Bonus/pipex_bonus.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_bonus.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: busseven <busseven@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/23 13:51:25 by busseven          #+#    #+#             */
+/*   Updated: 2025/01/26 14:57:32 by busseven         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <sys/wait.h>
+#include "../ft_printf/ft_printf.h"
+#include "../ft_printf/libft/libft.h"
+#include <fcntl.h>
+#include "pipex.h"
+#include <stdio.h>
+#include <signal.h>
+
+void	init_program(t_pipex *prog, char **argv, int argc, char **env)
+{
+	int i;
+
+	i = 0;
+	prog->fd_infile = open(argv[1], O_RDWR);
+	prog->fd_outfile = open(argv[argc - 1], O_RDWR);
+	prog->cmd_arr = ft_calloc(argc - 3, sizeof(t_cmd));
+	prog->paths = extract_env_path(env);
+	while(prog->cmd_arr[i])
+	{
+		prog->cmd_arr[i] = ft_calloc(1, sizeof(t_cmd));
+		prog->cmd_arr[i]->cmd_arr = make_command_arr(argv[i + 2]);
+		prog->cmd_arr[i]->path = find_correct_path(prog->cmd_arr[i]->arg_arr[0], prog->paths);
+		if (!prog->cmd_arr[i]->path) 
+			invalid_command(prog, prog->cmd_arr[i]->cmd_arr[0]);
+		i++;
+	}
+	i = 0;
+	prog->fd = ft_calloc(argc - 3, sizeof(int[2]));
+	while(prog->fd[i])
+		pipe(fd[i++]);
+	if (prog->fd_infile < 0 || prog->fd_outfile < 0)
+		invalid_file(prog);
+}
+
+void	process(int i, t_pipex *prog, char **env)
+{
+	if (i == 0)
+	{
+		dup2(prog->fd_infile, 0);
+		dup2(prog->fd[1], 1);
+		close(prog->fd[0]);
+		execve(prog->cmd_arr[0]->path, prog->cmd_arr[0]->arg_arr, env);
+	}
+	if (i == 1)
+	{
+		dup2(prog->fd_outfile, 1);
+		dup2(prog->fd[0], 0);
+		close(prog->fd[1]);
+		execve(prog->cmd_arr[1]->path, prog->cmd_arr[1]->arg_arr, env);
+	}
+}
+
+int	main(int argc, char **argv, char **env)
+{
+	t_pipex	*prog;
+	int		id;
+	int		i;
+
+	i = 0;
+	id = 1;
+	if (argc == 5)
+	{
+		prog = ft_calloc(1, sizeof(t_pipex));
+		init_program(prog, argv, env);
+		while (i <= 1)
+		{
+			if (id != 0)
+				id = fork();
+			if (id == 0)
+				process(i, prog, env);
+			else
+				ft_printf("parent\n");
+			i++;
+		}
+		wait(NULL);
+		free_prog(prog);
+	}
+	else
+		ft_printf("incorrect number of arguments\n");
+}

aad296ab43d0ee493ffea7ed06cf5c76105e8664
Büşra Seven <152426280+busras3v3n@users.noreply.github.com>
2025-01-26 11:55:28 +0300

    organized files

---
+++ Mandatory/pipex.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: busseven <busseven@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/23 13:21:45 by busseven          #+#    #+#             */
+/*   Updated: 2025/01/25 19:35:06 by busseven         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <stdlib.h>
+
+typedef struct s_cmd
+{
+	char	**arg_arr;
+	char	*path;
+}	t_cmd;
+
+typedef struct s_pipex
+{
+	int		fd_infile;
+	int		fd_outfile;
+	t_cmd	**cmd_arr;
+	char	**paths;
+	int		fd[2];
+}	t_pipex;
+
+void	free_prog(t_pipex *prog);
+void	invalid_file(t_pipex *prog);
+void	invalid_command(t_pipex *prog);
+char	**ft_freeall(char **arr);
+char	**extract_env_path(char **env);
+char	*find_correct_path(char *cmd_arr, char **paths);
+char	**make_command_arr(char *cmd);
+void	init_program(t_pipex *prog, char **argv, char **env);
+char	**ft_free_2d_arr(char **arr);
+
+#endif
